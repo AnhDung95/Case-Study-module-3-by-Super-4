2f2cd700de791dbc206560d55d5cc0ba58cad693
MinhHoang3011 <minhhoang301198@gmail.com>
2022-08-29 10:52:22 +0700

    create model

---
+++ src/main/java/com/example/casestudymodule3/model/OrderBill.java
@@ -0,0 +1,50 @@
+package com.example.casestudymodule3.model;
+
+public class OrderBill {
+    private int orderId;
+    private int bookId;
+    private int quantity;
+    private double total;
+
+    public OrderBill() {
+    }
+
+    public OrderBill(int orderId, int bookId, int quantity, double total) {
+        this.orderId = orderId;
+        this.bookId = bookId;
+        this.quantity = quantity;
+        this.total = total;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+}
