e3274ec00572bd4a7a4fb905c970621add351fcf
AnhDung95 <nguyendung2172013@gmail.com>
2022-08-31 09:11:57 +0700

    dung fix 1

--- src/main/java/com/example/casestudymodule3/dao/UsersDaoImplement.java
+++ src/main/java/com/example/casestudymodule3/dao/UsersDaoImplement.java
@@ -41,7 +41,7 @@ public class UsersDaoImplement implements IUsersDAO {
                 String address = rs.getString(6);
                 String phone = rs.getString(7);
                 String email = rs.getString(8);
-                Role role = Role.valueOf(rs.getString(9);
+                Role role = Role.valueOf(rs.getString(9));
                 Status status = Status.valueOf(rs.getString(10));
                 users.add(new Users(id, account, password, firstname, lastname, address, phone, email, role));
             }
@@ -50,7 +50,122 @@ public class UsersDaoImplement implements IUsersDAO {
         }
         return users;
     }
-public String findPassByAccount
+
+    public boolean add(Users users) {
+        boolean rowAdded = false;
+        try {
+            PreparedStatement statement = connection.prepareStatement(QUERY_INSERT_ACCOUNT_BY_ADMIN);
+            statement.setString(1, users.getAccount());
+            statement.setString(2, users.getPassword());
+            statement.setString(3, users.getFirstName());
+            statement.setString(4, users.getLastName());
+            statement.setString(5, users.getAddress());
+            statement.setString(6, users.getPhone());
+            statement.setString(7, users.getEmail());
+            statement.setString(8, String.valueOf(users.getRole()));
+            rowAdded = statement.executeUpdate() > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return rowAdded;
+    }
+
+    public boolean update(int id, Users users) {
+        boolean rowUpdate = false;
+        try {
+            PreparedStatement statement = connection.prepareStatement(QUERY_UPDATE_BY_ADMIN);
+            statement.setInt(1,id);
+            rowUpdate = statement.executeUpdate() > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return rowUpdate;
+    }
+
+    public boolean delete(int id) {
+        boolean rowDel = false;
+        try {
+            PreparedStatement statement = connection.prepareStatement(QUERY_DEL_USERS_BY_ADMIN);
+            statement.setInt(1,id);
+            rowDel = statement.executeUpdate() > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return rowDel;
+    }
+
+    public Users findById(int id) {
+        Users user = null;
+        try {
+            PreparedStatement statement = connection.prepareStatement(QUERY_FIND_BY_ID);
+            statement.setInt(1,id);
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()){
+                String account = rs.getString(2);
+                String password = rs.getString(3);
+                String firstname = rs.getString(4);
+                String lastname = rs.getString(5);
+                String address = rs.getString(6);
+                String phone = rs.getString(7);
+                String email = rs.getString(8);
+                Role role = Role.valueOf(rs.getString(9));
+                user = new Users(id,account,password,firstname,lastname,address,phone,email,role);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return user ;
+    }
+    public String findPassByAccount(String account, String email) {
+        String pass = "";
+        try {
+            PreparedStatement statement = connection.prepareStatement(QUERY_FIND_PASS_BY_ACCOUNT);
+            statement.setString(1,account);
+            statement.setString(2,email);
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()){
+                String password = rs.getString(1);
+                pass = password;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return pass;
+    }
+    public boolean updateByUser(String account,Users users) {
+        boolean rowUpdate = false;
+        try {
+            PreparedStatement statement = connection.prepareStatement(QUERY_UPDATE_BY_USER);
+            statement.setString(1, users.getPassword());
+            statement.setString(2,users.getFirstName());
+            statement.setString(3,users.getLastName());
+            statement.setString(4,users.getAddress());
+            statement.setString(5,users.getPhone());
+            statement.setString(6,users.getEmail());
+            statement.setString(7,account);
+            rowUpdate = statement.executeUpdate() > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return rowUpdate;
+    }
+
+    @Override
+    public int findByUser(Users users) {
+        int id = -1;
+        try {
+            PreparedStatement statement = connection.prepareStatement(QUERY_FIND_BY_USER);
+            statement.setString(1, users.getAccount());
+            statement.setString(2, users.getPassword());
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                id = rs.getInt(1);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return id;
+    }
 
 
 
--- src/main/java/com/example/casestudymodule3/service/implementService/UsersServiceImplement.java
+++ src/main/java/com/example/casestudymodule3/service/implementService/UsersServiceImplement.java
@@ -1,9 +1,53 @@
 package com.example.casestudymodule3.service.implementService;
 
+import com.example.casestudymodule3.dao.UsersDaoImplement;
+import com.example.casestudymodule3.dao.interfaceDao.IUserDao;
 import com.example.casestudymodule3.model.Users;
 import com.example.casestudymodule3.service.interfaceService.IUsersService;
 
 import java.util.List;
 
 public class UsersServiceImplement implements IUsersService {
+    private final IUserDao usersDao = (IUserDao) new UsersDaoImplement();
+
+    @Override
+    public List<Users> getAll() {
+        return usersDao.getAll();
+    }
+
+    @Override
+    public boolean add(Users users) {
+        return usersDao.add(users);
+    }
+
+    @Override
+    public boolean update(int id, Users users) {
+        return usersDao.update(id,users);
+    }
+
+    @Override
+    public boolean delete(int id) {
+        return usersDao.delete(id);
+    }
+
+    @Override
+    public Users findById(int id) {
+        return usersDao.findById(id);
+    }
+
+    @Override
+    public String findPassByAccount(String account, String email) {
+        return usersDao.findPassByAccount(account,email);
+    }
+
+    @Override
+    public boolean updateByUser(String account, Users users) {
+        return usersDao.updateByUser(account,users);
+    }
+
+    @Override
+    public int findByUser(Users users) {
+        return usersDao.findByUser(users);
+    }
 }
+
